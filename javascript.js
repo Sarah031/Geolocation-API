e5d95afaf0d6aba6889afc0056575c3a0f004619
Sara Majstorović <sara.majstorovic01@gmail.com>
2023-08-31 21:13:12 +0200

    new

--- javascript.js
+++ javascript.js
@@ -2,12 +2,14 @@ const ipData = document.querySelector("#ip");
 const countryData = document.querySelector("#country");
 const cityData = document.querySelector("#city");
 
-fetch("http://ip-api.com/json/?fields=61439")
+fetch(
+  "http://api.ipstack.com/95.168.121.26?access_key=4d1e7095c018da932ccf5e4f3753023b"
+)
   .then((res) => res.json())
   .then((res) => {
-    ipData.textContent = "IP: " + res.query;
-    countryData.textContent = "Country: " + res.country;
+    ipData.textContent = "IP: " + res.ip;
+    countryData.textContent = "Country: " + res.country_name;
     sessionStorage.setItem("country", res.country);
     cityData.textContent = "City: " + res.city;
-    console.log(res.query, res.country, res.city);
+    console.log(res.query, res.country_name, res.city);
   });
